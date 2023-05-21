d696848150476b2608a9cb4be1a479aba86e947a
Aakash Dhakal <53038685+aakashdhakal@users.noreply.github.com>
2023-05-21 20:43:00 +0545

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,49 @@
+<section id="footer">
+    <div class="max-width">
+        <div class="left-content">
+            <div class="logo-container">
+                <img src="./aakashdhakal.png" alt="">
+                <h1>Blogs</h1>
+
+            </div>
+            <p>A online garbage bin created because the developer was bored to dump our garbage thought. Don,t worry if you dump your thoughts here. Nobody visits even if the site is online. If you want to contact the developer/owner then below are his social links</p>
+            <div class="social-links">
+                <a href="https://facebook.com/dhakallaakash"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://instagram.com/aakash_dhakal12"><i class="fab fa-instagram"></i></a>
+                <a href="https://twitter.com/aakash_dhakal12"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.linkedin.com/in/aakash-dhakal12"><i class="fab fa-linkedin-in"></i></a>
+                <a href="https://github.com/aakashdhakal"><i class="fab fa-github"></i></a>
+            </div>
+
+        </div>
+        <div class="middle-content">
+            <h3>Quick Links</h3>
+            <ul class="nav-links">
+                <li><a href=""> Home</a></li>
+                <li><a href="">Blogs</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Authors</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </div>
+        <div class="right-content">
+            <h3>Want ro dump content?</h3>
+            <p>Dump your content in this site for free (applies to both parties). Send me your email and I will check back to you soon (Just acting busy)</p>
+
+            <div class="send-email">
+                <input type="email" placeholder="Enter your email">
+                <button id="sendBtn">Send</button>
+            </div>
+        </div>
+
+
+
+    </div>
+    <div class="bottom-content">
+        <p>&copy; <?php echo date("Y"); ?> <a href="https://aakashdhakal.com.np">Aakash Dhakal</a> . All rights reserved</p>
+        <div class="privacy-policy">
+            <a href="">Privacy Policy</a>
+            <a href="">Terms & Conditions</a>
+        </div>
+    </div>
+</section>
